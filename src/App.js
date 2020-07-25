83200b4df5eb9888f9b60d76fe68d36d5aabcb2a
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-07-25 16:19:42 +1200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import TrelloList from './components/TrelloList'
+import TrelloActionButton from './components/TrelloActionButton'
+import { sort } from './actions'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+class App extends Component {
+
+  // fires when anything draggable is engaged
+  onDragEnd = (result) => {
+    // destination and source house props droppableId and index
+    const { destination, source, draggableId, type } = result
+
+    // check if there is a destination
+    if(!destination) {
+      return
+    }
+
+    this.props.dispatch(
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId,
+        type
+      )
+    )
+  }
+
+  render() {
+    const { lists } = this.props
+
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <h2>Redux Trello Clone</h2>
+        <Droppable droppableId="all-lists" direction="horizontal" type="list">
+          {provided => (
+            <ListContainer {...provided.droppableProps} ref={provided.innerRef}>
+              {lists.map((list, index) => (
+                <TrelloList 
+                  listID={list.id} 
+                  key={list.id} 
+                  title={list.title} 
+                  cards={list.cards} 
+                  index={index}
+                />
+              ))} 
+              {provided.placeholder}
+              <TrelloActionButton list /> 
+            </ListContainer>
+          )}
+        </Droppable>
+        
+      </DragDropContext>
+    )
+  } 
 }
 
-export default App;
+const mapStateToProps = state => ({
+  lists: state.lists
+})
+
+export default connect(mapStateToProps)(App) 
---
+++ src/components/TrelloList.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Droppable, Draggable } from 'react-beautiful-dnd'
+import TrelloCard from './TrelloCard'
+import TrelloActionButton from './TrelloActionButton'
+import styled from 'styled-components'
+
+// styles
+const ListContainer = styled.div`
+  background-color: #dfe3e6;
+  border-radius: 3px;
+  width: 300px;
+  height: 100%;
+  padding: 8px;
+  margin-right: 8px;
+`
+
+const TrelloList = ({ title, cards, listID, index }) => {
+  return (
+    // everything inside of Droppable uses render props
+    <Draggable draggableId={String(listID)} index={index}>
+      {provided => (
+        <ListContainer 
+          {...provided.draggableProps} 
+          ref={provided.innerRef} 
+          {...provided.dragHandleProps}
+        >
+          <Droppable droppableId={String(listID)}>
+            {provided => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                <h4>{title}</h4>
+                {cards.map((card, index) => (
+                  <TrelloCard 
+                    index={index} 
+                    key={card.id} 
+                    text={card.text} 
+                    id={card.id} 
+                  />
+                ))}
+                {provided.placeholder}
+                <TrelloActionButton listID={listID} />
+              </div>
+            )}
+          </Droppable>
+        </ListContainer>
+      )}
+    </Draggable>
+    
+  )
+}
+
+export default TrelloList
